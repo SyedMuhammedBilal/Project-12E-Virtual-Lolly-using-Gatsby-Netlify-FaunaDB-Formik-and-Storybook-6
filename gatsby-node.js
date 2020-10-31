75db8db3e3117de49ac25ef48c0ff8a186673db8
Syed Muhammed Bilal <intergo878@gmail.com>
2020-10-31 19:19:19 +0500

    Add files via upload

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,37 +1,38 @@
-const path = require("path")
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
+// const path = require("path")
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPages } = actions
 
-  const { data } = await graphql(`
-    {
-      getVCard {
-        getVCard {
-          id,
-          c1,
-          c2,
-          c3,
-          link,
-          rec,
-          sender,
-          msg
-        }
-      }
-    }
-  `)
+//   const { data } = await graphql(`
+//   {
+//     get_lollies {
+//       getVCard {
+//         c1
+//         c2
+//         c3
+//         link
+//         rec
+//         msg
+//         sender
+//       }
+//     }
+//   }
+  
+//   `)
+//   console.log(data)
 
-  data.getVCard.getVCard.forEach(node => {
-    createPage({
-      path: `lolly/${node.link}`,
-      component: path.resolve("./src/templates/template.tsx"),
-      context: {
-        c1: node.c3,
-        c2: node.c2,
-        c3: node.c1,
-        link: node.link,
-        msg: node.msg,
-        sender: node.sender,
-        rec: node.rec,
-      },
-    })
-  })
-}
+//   data.get_lollies.getVCard.forEach(node => {
+//     createPages({
+//       path: `lolly/${node.link}`,
+//       component: path.resolve("./src/templates/template.tsx"),
+//       context: {
+//         top: node.c1,
+//         middle: node.c2,
+//         bottom: node.c3,
+//         link: node.link,
+//         msg: node.msg,
+//         sender: node.sender,
+//         rec: node.rec,
+//       },
+//     })
+//   })
+// }
