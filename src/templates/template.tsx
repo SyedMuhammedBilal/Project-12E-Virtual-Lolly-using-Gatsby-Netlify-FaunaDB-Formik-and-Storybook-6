5d60c7d223517dd2d8b09c3a2c750dfd3e5787e9
Syed Muhammed Bilal <intergo878@gmail.com>
2020-10-31 19:20:28 +0500

    Add files via upload

--- src/templates/template.tsx
+++ src/templates/template.tsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import Header from '../components/Header'
-import { Lolly } from '../components/Lolly'
-import Result from '../components/Result'
+// import React from 'react'
+// import Header from '../components/Header'
+// import { Lolly } from '../components/Lolly'
+// import Result from '../components/Result'
 
-const Template = ({ pageContext: { top, middle, bottom, rec, sender, msg, link } }) => {
+// const Template = ({ pageContext: { top, middle, bottom, rec, sender, msg, link } }) => {
+//     console.log(top, middle, bottom, rec, sender, msg, link);
+//     return (
+//         <div>
+//             <Header />
+//             <div className="lollyFormDiv">
+//                 <div>
+//                     <Lolly top={top} middle={middle} bottom={bottom} />
+//                 </div>
+//                 <Result link={link} rec={rec} sender={sender} msg={msg} />
+//             </div>
+//         </div>
+//     )
+// }
 
-    return (
-        <div>
-            <Header />
-            <div className="lollyFormDiv">
-
-                <div>
-                    <Lolly top={top} middle={middle} bottom={bottom} />
-                </div>
-
-                <Result link={link} rec={rec} sender={sender} msg={msg} />
-            </div>
-        </div>
-    )
-}
-
-export default Template
+// export default Template;
